6c1bdb1b58df2defbfa09d850f666fb642f11cc5
Bruno casotto <casottoalves@hotmail.com>
2018-01-30 02:08:31 -0200

    Bootstrap dependencie added

--- webpack/aliases.js
+++ webpack/aliases.js
@@ -2,10 +2,10 @@ const path = require('path')
 
 module.exports = {
   _components: path.resolve(__dirname, '..', 'src', 'app', 'components'),
-  _services:   path.resolve(__dirname, '..', 'src', 'app', 'services'),
-  _helpers:    path.resolve(__dirname, '..', 'src', 'app', 'helpers'),
-  _routes:     path.resolve(__dirname, '..', 'src', 'app', 'routes'),
-  _sass:       path.resolve(__dirname, '..', 'src', 'assets', 'sass'),
-  _npm:        path.join(__dirname, '..', 'node_modules'),
-  'vue$':      path.join(__dirname, '..', 'node_modules', 'vue/dist/vue.common.js')
+  _services: path.resolve(__dirname, '..', 'src', 'app', 'services'),
+  _helpers: path.resolve(__dirname, '..', 'src', 'app', 'helpers'),
+  _routes: path.resolve(__dirname, '..', 'src', 'app', 'routes'),
+  _sass: path.resolve(__dirname, '..', 'src', 'assets', 'sass'),
+  _node_modules: path.join(__dirname, '..', 'node_modules'),
+  'vue$': path.join(__dirname, '..', 'node_modules', 'vue/dist/vue.common.js')
 }
